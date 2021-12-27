4f523ccfdb656837cf2128e419113f157ab39c1a
Kaminsky <kaminskyfrontend@gmail.com>
2021-12-27 13:38:11 +0200

    less01

--- script/index.js
+++ script/index.js
@@ -1,6 +1,22 @@
 'use strict';
+// Создайте новый репозиторий для проекта crm
+// Создайте ветку dev и в версию dev загрузите файлы из архива crm.zip, так как показано во втором видео
+// Создайте новую ветку для этого урока, подключите js файл к странице и в константы получите 4 элемента из модального окна
+// 1. Заголовок 
+// 2. Форма
+// 3. Чекбокс
+// 4. Поле рядом с чекбоксом
+// Именование констант должно быть говорящем за себя, по имени должно быть понятно какой элемент в константе хранится. 
+// На проверку присылайте ссылку на текущую ветку, после принятия работы куратором ветку сливаем в ветку dev и исходную ветку удаляем.
 
 
-const sum = (a, b) => a + b;
-
-sum(3, 3);
+const modalTitle = document.querySelector('.modal__title')
+console.log('modalTitle: ', modalTitle);
+const modalForm = document.querySelector('.modal__form')
+console.log('modalForm: ', modalForm);
+const formTitle = modalForm.querySelector('#name')
+console.log('formTitle: ', formTitle);
+const formDiscountCheckbox = modalForm.querySelector('#discount')
+console.log('formDiscountCheckbox: ', formDiscountCheckbox);
+const formDiscountCount = modalForm.querySelector('[name="discount_count"]')
+console.log('formDiscountCount: ', formDiscountCount);
